ff77419ceba00040c4e960c88f62514a062ed445
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-06-09 21:37:01 -0500

    feat: made changes per levelup updates instructions

--- src/utils/context/ViewDirector.js
+++ src/utils/context/ViewDirector.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import RegisterForm from '@/components/RegisterForm';
 import { useAuth } from '@/utils/context/authContext';
 import Loading from '@/components/Loading';
 import SignIn from '@/components/SignIn';
 import NavBar from '@/components/NavBar';
 
 function ViewDirectorBasedOnUserAuthStatus({ children }) {
-  const { user, userLoading } = useAuth();
+  const { user, userLoading, updateUser } = useAuth();
 
   // if user state is null, then show loader
   if (userLoading) {
@@ -17,7 +18,7 @@ function ViewDirectorBasedOnUserAuthStatus({ children }) {
     return (
       <>
         <NavBar /> {/* NavBar only visible if user is logged in and is in every view */}
-        {children}
+        <div className="container">{'valid' in user ? <RegisterForm user={user} updateUser={updateUser} /> : children}</div>
       </>
     );
   }
